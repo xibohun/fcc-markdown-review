43e6e891290fd96ec35fcc0d7b5a81573b0f3014
liuzhi <james_73@hotmail.com>
2022-12-07 11:12:58 -0700

    markdown

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from "react-dom"
+import "./App.css"
+
+class App extends React.Component{
+    render(){
+        return(
+            <div> new version</div>
+        )
+    }
+}
+
+export default App
